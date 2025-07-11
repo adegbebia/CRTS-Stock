816f33948d3f6905920f9dc3e51bfccee4d47931
degbebia <traoredegbebia@gmail.com>
2025-07-11 11:05:40 +0100

    modification

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
 
 
 
-        $this->call(UserSeeder::class);
+       $this->call([
+            UserSeeder::class, 
+        ]);
         // User::factory(10)->create();
 
         // User::factory()->create([
