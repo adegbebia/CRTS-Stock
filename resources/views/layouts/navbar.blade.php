21a32dfafca1a96b1535363c890e8f04bf8fe7d9
degbebia <traoredegbebia@gmail.com>
2025-08-13 10:16:04 +0100

    fin du disigne

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -44,16 +44,16 @@
         </div>
     </div>
     <div class="navbar-center">
-        <a class="btn btn-ghost text-xl">crts-stock</a>
+        <a class="btn btn-ghost text-3xl">CRTS-STOCK</a>
     </div>
     <div class="navbar-end">
-        <button class="btn btn-ghost btn-circle">
+        {{-- <button class="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
-        </button>
+        </button> --}}
         <style>
             [x-cloak] {
                 display: none !important;
@@ -106,7 +106,7 @@
                                         'type' => $alerte->typealerte,
                                     ])
                                 </div>
-                                <small class="text-gray-400">
+                                <small class="text-gray-400 text-xl">
                                     Déclenché le {{ $alerte->datedeclenchement->format('d/m/Y H:i') }}
                                 </small>
                             </a>
@@ -139,7 +139,7 @@
                             (!$user->hasRole('admin') &&
                                 !$user->hasRole('magasinier_technique') &&
                                 !$user->hasRole('magasinier_collation')))
-                        <div class="px-4 py-3 text-gray-500 text-center select-none">
+                        <div class="px-4 py-3 text-gray-500 text-center select-none ">
                             Pas de notifications
                         </div>
                     @endif
@@ -156,21 +156,13 @@
                 </div>
             </div>
             <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                <li>
-                    <a class="justify-between">
-                        Profile
-                        <span class="badge">New</span>
-                    </a>
-                </li>
-
-
                 <!-- <form action="{{ route('logout') }}" method="POST">
                     @csrf
                     <span class="badge">Logout</span>
                 </form> -->
 
                 <li>
-                    <form action="{{ route('logout') }}" method="POST">
+                    <form action="{{ route('logout') }}" method="POST" class=" bg-red-200">
                         @csrf
                         <button type="submit">
                             Logout
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,6 @@
-<div id="sidebar" class="fixed top-0 left-0 w-64 h-[calc(68.5vh-0rem)] 
-           bg-red-200 p-6  transition-all duration-300 
+<div id="sidebar"
+    class="fixed top-0 bottom-0 left-0 w-64 
+           bg-red-200 p-6 transition-all duration-300 
            overflow-y-auto border-r border-blue-300">
     <ul class="space-y-4 text-blue-900">
 
